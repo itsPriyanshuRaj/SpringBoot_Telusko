07b45d6e54800bb8edfef33e43a5aa218932f8c7
itsPriyanshuRaj <priyanshuraj14raj@gmail.com>
2025-03-08 00:25:18 +0530

    blank responses on api resolved

--- BoilerPlate/src/main/java/com/priyanshuraj/demo/modal/Task.java
+++ BoilerPlate/src/main/java/com/priyanshuraj/demo/modal/Task.java
@@ -17,73 +17,72 @@ import java.util.Date;
 
 @Document(collection="task")
 public class Task {
-//    public Task(String id, String taskName, String taskDesc, Instant taskDueDate, String taskStatus, Instant taskCreationDate, Instant taskModifiedDate) {
-//        this.id = id;
-//        this.taskName = taskName;
-//        this.taskDesc = taskDesc;
-//        this.taskDueDate = taskDueDate;
-//        this.taskStatus = taskStatus;
-//        this.taskCreationDate = taskCreationDate;
-//        this.taskModifiedDate = taskModifiedDate;
-//    }
+    public Task(String id, String taskName, String taskDesc, Instant taskDueDate, String taskStatus, Instant taskCreationDate, Instant taskModifiedDate) {
+        this.id = id;
+        this.taskName = taskName;
+        this.taskDesc = taskDesc;
+        this.taskDueDate = taskDueDate;
+        this.taskStatus = taskStatus;
+        this.taskCreationDate = taskCreationDate;
+        this.taskModifiedDate = taskModifiedDate;
+    }
 
+    
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
 
-//    public String getId() {
-//        return id;
-//    }
-//
-//    public void setId(String id) {
-//        this.id = id;
-//    }
-//
-//    public String getTaskName() {
-//        return taskName;
-//    }
-//
-//    public void setTaskName(String taskName) {
-//        this.taskName = taskName;
-//    }
-//
-//    public String getTaskDesc() {
-//        return taskDesc;
-//    }
-//
-//    public void setTaskDesc(String taskDesc) {
-//        this.taskDesc = taskDesc;
-//    }
-//
-//    public Instant getTaskDueDate() {
-//        return taskDueDate;
-//    }
-//
-//    public void setTaskDueDate(Instant taskDueDate) {
-//        this.taskDueDate = taskDueDate;
-//    }
-//
-//    public String getTaskStatus() {
-//        return taskStatus;
-//    }
-//
-//    public void setTaskStatus(String taskStatus) {
-//        this.taskStatus = taskStatus;
-//    }
-//
-//    public Instant getTaskCreationDate() {
-//        return taskCreationDate;
-//    }
-//
-//    public void setTaskCreationDate(Instant taskCreationDate) {
-//        this.taskCreationDate = taskCreationDate;
-//    }
-//
-//    public Instant getTaskModifiedDate() {
-//        return taskModifiedDate;
-//    }
-//
-//    public void setTaskModifiedDate(Instant taskModifiedDate) {
-//        this.taskModifiedDate = taskModifiedDate;
-//    }
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskDesc() {
+        return taskDesc;
+    }
+
+    public void setTaskDesc(String taskDesc) {
+        this.taskDesc = taskDesc;
+    }
+
+    public Instant getTaskDueDate() {
+        return taskDueDate;
+    }
+
+    public void setTaskDueDate(Instant taskDueDate) {
+        this.taskDueDate = taskDueDate;
+    }
+
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public Instant getTaskCreationDate() {
+        return taskCreationDate;
+    }
+
+    public void setTaskCreationDate(Instant taskCreationDate) {
+        this.taskCreationDate = taskCreationDate;
+    }
+
+    public Instant getTaskModifiedDate() {
+        return taskModifiedDate;
+    }
+
+    public void setTaskModifiedDate(Instant taskModifiedDate) {
+        this.taskModifiedDate = taskModifiedDate;
+    }
     @Id
     private String id;
     private String taskName;
