c75c8edd348f534e1eeaf90e8c384e26fae0f698
itsPriyanshuRaj <priyanshuraj14raj@gmail.com>
2025-03-24 22:40:33 +0530

    Adding spring security projectL

---
+++ Spring-security-with-Gemini/src/main/java/com/priyanshuRaj/Spring/security/with/Gemini/SpringSecurityWithGeminiApplication.java
@@ -0,0 +1,13 @@
+package com.priyanshuRaj.Spring.security.with.Gemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithGeminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithGeminiApplication.class, args);
+	}
+
+}
---
+++ Spring-security-with-Gemini/src/test/java/com/priyanshuRaj/Spring/security/with/Gemini/SpringSecurityWithGeminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyanshuRaj.Spring.security.with.Gemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithGeminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
