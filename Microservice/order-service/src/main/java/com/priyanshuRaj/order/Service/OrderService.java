9bae3dc632863f6fc1ad04851d7ce801ffb1fb76
itsPriyanshuRaj <priyanshuraj14raj@gmail.com>
2025-05-01 16:27:59 +0530

    Client/InventoryClient

--- Microservice/order-service/src/main/java/com/priyanshuRaj/order/Service/OrderService.java
+++ Microservice/order-service/src/main/java/com/priyanshuRaj/order/Service/OrderService.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.priyanshuRaj.order.client.InventoryClient;
 import com.priyanshuRaj.order.dto.OrderRequest;
 import com.priyanshuRaj.order.model.Order;
 import com.priyanshuRaj.order.repository.OrderRepo;
@@ -16,13 +17,23 @@ import lombok.AllArgsConstructor;
 public class OrderService {
 
     private final OrderRepo orderRepo;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest){
-        Order order = new Order();
+
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.Quantity());
+
+        if(isProductInStock){
+            Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         order.setPrice(orderRequest.price());
         order.setQuantity(orderRequest.Quantity());
         order.setSkuCode(orderRequest.skuCode());
         orderRepo.save(order);
+        }else{
+            throw new RuntimeException("Product is out of stock" + orderRequest.skuCode());
+        }
+
+        
     }
 }
